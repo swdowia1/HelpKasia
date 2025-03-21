67b84ea756171095f94be06cfc9e18435431fce1
Sławomir Wdowiak <slawomir.wdowiak@onninen.com>
2025-03-21 14:40:49 +0100

    polecenia i zamkniecia w pliku

--- HelpKasia/Form1.Designer.cs
+++ HelpKasia/Form1.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.chkZwin = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
@@ -40,63 +37,31 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoSize = true;
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(16, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(937, 32);
+            this.panel1.Size = new System.Drawing.Size(1688, 39);
             this.panel1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(982, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(1177, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Zwin";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(1096, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Zamknij";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // chkZwin
             // 
             this.chkZwin.AutoSize = true;
-            this.chkZwin.Location = new System.Drawing.Point(1054, 35);
+            this.chkZwin.Location = new System.Drawing.Point(1580, 61);
+            this.chkZwin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkZwin.Name = "chkZwin";
-            this.chkZwin.Size = new System.Drawing.Size(94, 17);
+            this.chkZwin.Size = new System.Drawing.Size(114, 20);
             this.chkZwin.TabIndex = 5;
             this.chkZwin.Text = "zwin do paska";
             this.chkZwin.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1276, 52);
+            this.ClientSize = new System.Drawing.Size(1707, 84);
             this.Controls.Add(this.chkZwin);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
@@ -110,9 +75,6 @@
 
         #endregion
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.CheckBox chkZwin;
     }
 }
--- HelpKasia/Form1.cs
+++ HelpKasia/Form1.cs
@@ -14,36 +14,64 @@ namespace HelpKasia
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 
             CreateButtons();
-            label1.Text = "";
+
 
 
         }
 
         private void CreateButtons()
         {
+            int buttonWidth = 100;
+            int buttonHeight = 30;
+            int padding = 10; // Odstęp między przyciskami
+            int buttonsPerRow = panel1.Width / (buttonWidth + padding);
             string ff = Environment.CurrentDirectory;
             string[] pliki = Directory.GetFiles(Environment.CurrentDirectory, "*.txt");
-            // Number of buttons to create (for example, 5 buttons)
-            int numberOfButtons = 5;
 
+            int lastI = 0;
             // Loop to create buttons
             for (int i = 0; i < pliki.Length; i++)
             {
-                // Create a new Button instance
+                lastI = i;
                 Button newButton = new Button();
                 var filename = Path.GetFileName(pliki[i]);
-                // Set button properties
+
                 newButton.Text = filename;
-                newButton.Name = "btnDynamic" + i; // Button name
-                newButton.Width = 100; // Button width
-                newButton.Height = 30; // Button height
-                newButton.Location = new System.Drawing.Point(i * 100, 0);
-                // Attach a common event handler for button clicks
+                newButton.Name = "btnDynamic" + i;
+                newButton.Width = buttonWidth;
+                newButton.Height = buttonHeight;
+
+                // Obliczenie pozycji wiersza i kolumny
+                int row = i / buttonsPerRow; // Określenie rzędu
+                int col = i % buttonsPerRow; // Określenie kolumny
+
+                newButton.Location = new System.Drawing.Point(col * (buttonWidth + padding), row * (buttonHeight + padding));
+
                 newButton.Click += help_Click;
 
                 // Add the button to the panel
                 panel1.Controls.Add(newButton);
             }
+            lastI++;
+            //zamykanie
+            Button newButton1 = new Button();
+
+
+            newButton1.Text = "Zamknij";
+            newButton1.Name = "btnDynamic";
+            newButton1.Width = buttonWidth;
+            newButton1.Height = buttonHeight;
+
+            // Obliczenie pozycji wiersza i kolumny
+            int row1 = lastI / buttonsPerRow; // Określenie rzędu
+            int col1 = lastI % buttonsPerRow; // Określenie kolumny
+
+            newButton1.Location = new System.Drawing.Point(col1 * (buttonWidth + padding), row1 * (buttonHeight + padding));
+
+            newButton1.Click += close_click;
+
+            // Add the button to the panel
+            panel1.Controls.Add(newButton1);
 
 
         }
@@ -69,7 +97,7 @@ namespace HelpKasia
         private void help_Click(object sender, EventArgs e)
         {
             var b = sender as Button;
-            label1.Text = "wybrano " + b.Text;
+
             string popText = b.Text;
             if (b != null)
             {
@@ -96,7 +124,7 @@ namespace HelpKasia
             this.WindowState = FormWindowState.Minimized;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void close_click(object sender, EventArgs e)
         {
             DialogResult dialogResult = MessageBox.Show("Czy na pewno zamknac", "Zamknac", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             if (dialogResult == DialogResult.Yes)
